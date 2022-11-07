dfea59907f7938d82afd5a41ae220cd8ff534702
Kalinga <kalingakhatua@gmail.com>
2022-11-07 19:12:56 +0530

    added javascript for cards

--- js/script.js
+++ js/script.js
@@ -1,3 +1,47 @@
-console.log('Hemlo');
+console.clear()
 
-cards = document.querySelector("featured__cards");
+console.log('Hemlo');
+const formatter = Intl.NumberFormat('en',
+{
+    notation: 'standard',
+    style: 'currency',
+    currency: 'INR',
+})
+
+let cards = document.querySelector(".featured__cards");
+console.log(cards);
+
+async function getData() {
+    // let prods = await fetch("assets/products.json");
+    let prods = await fetch("https://dummyjson.com/products/category/womens-jewellery");
+    // let prods = await fetch("https://dummyjson.com/products/category/furniture");
+    let data = await prods.json();
+    return data;
+}
+
+async function main() {
+    let prodData = await getData();
+    console.log(prodData);
+
+    // console.log(prodData.products);
+
+    for (const prod of prodData.products) {
+        console.log(prod)
+        cards.innerHTML += `
+        <div class="featured-card">
+          <div class="featured-card-image-wrapper">
+            <div
+              class="featured-card-image"
+              style="
+                background-image: url('${prod.thumbnail}');
+              "
+            ></div>
+          </div>
+          <div class="featured-card-name">${prod.title}</div>
+          <div class="featured-card-price">${formatter.format(prod.price)}</div>
+        </div>
+        `
+    }
+}
+
+main()
